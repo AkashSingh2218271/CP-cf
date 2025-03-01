0d1dff8435850da89fe1747063fdecea38a1ac92
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-01 20:47:38 +0530

    Adding file Fast_math.cpp

--- codeforces_800_/Fast_math.cpp
+++ codeforces_800_/Fast_math.cpp
@@ -3,24 +3,24 @@
 using namespace std;
 
 int main() {
-    // Using bitset to read binary numbers
-    string input1, input2;
-    cin >> input1 >> input2;
+    // Read binary numbers as strings
+    string s1, s2;
+    cin >> s1 >> s2;
 
-    // Determine the size of the input (bit length)
-    int inputSize = input1.size(); // or use input2.size() since both should be the same length
+    // ans to store the XOR operation
+    string ans = "";
 
-    // Convert binary strings to bitsets of size 32 (maximum size here is 32, change as per input needs)
-    bitset<32> a(input1), b(input2);
+    // Perform XOR on each corresponding digit
+    for (int i = 0; i < s1.size(); ++i) {
+        if (s1[i] == s2[i]) {
+            ans += "0";  // Same bits, ans is 0
+        } else {
+            ans += "1";  // Different bits, ans is 1
+        }
+    }
 
-    // XOR the two bitsets
-    int ans = (a.to_ulong()) ^ (b.to_ulong());
-
-    // Convert the result to a bitset of the same size as input1
-    bitset<32> result(ans);
-
-    // Print the result as a bitset, but only display the relevant number of bits (equal to inputSize)
-    cout << result.to_string().substr(32 - inputSize) << endl;
+    // Output the ans
+    cout << ans << endl;
 
     return 0;
 }
