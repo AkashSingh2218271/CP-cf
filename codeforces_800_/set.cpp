adc22953d40c656be20e28b62ba9d3357ca895fa
AkashSingh2218271 <akrawat0056@gmail.com>
2025-03-18 21:22:03 +0530

    adding modification to set.cpp

--- codeforces_800_/set.cpp
+++ codeforces_800_/set.cpp
@@ -2,9 +2,6 @@
 #include <vector>
 using namespace std;
 
-int count_multiples(int x, int l, int r) {
-    return r / x - (l - 1) / x;
-}
 
 int main() {
     int t;
@@ -13,14 +10,9 @@ int main() {
     while (t--) {
         int l, r, k;
         cin >> l >> r >> k;
-        int ans = 0;
 
-        for (int x = l; x <= r; x++) {
-            int num_multiples = count_multiples(x, l, r);
-            if (num_multiples >= k) {
-                ans++;
-            }
-        }
+        int greatest_number = r / k;
+        int ans = greatest_number - l + 1;
 
         cout << ans << endl;
     }
