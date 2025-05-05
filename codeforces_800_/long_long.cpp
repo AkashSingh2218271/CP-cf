e4852d666a4b2878bb1347c893f1228347347ffa
AkashSingh2218271 <akrawat0056@gmail.com>
2025-05-05 20:53:43 +0530

    Adding file long_long.cpp

--- codeforces_800_/long_long.cpp
+++ codeforces_800_/long_long.cpp
@@ -3,70 +3,6 @@
 #include <cmath>
 using namespace std;
 
-bool check1(vector<int>& a, int n)
-{   
-    int idx = -1;
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] >= 0)
-        {
-            continue;
-        }
-        else
-        {
-            idx = i;
-        }
-    }
-
-    if (idx != -1)
-    {
-        for (int j = idx; j < n; j++)
-        {
-            if (a[j] <= 0)
-            {
-                continue;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-bool check2(vector<int>& a, int n)
-{
-    int idx = -1;
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] <= 0)
-        {
-            continue;
-        }
-        else
-        {
-            idx = i;
-        }
-    }
-    if (idx != -1)
-    {
-        for (int j = idx; j < n; j++)
-        {
-            if (a[j] >= 0)
-            {
-                continue;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
 int main()
 {
     int t;
@@ -77,33 +13,31 @@ int main()
         int n;
         cin >> n;
         vector<int> a(n);
-        int count_p = 0, count_n = 0;
+        int count = 0;
         long long sum = 0;
 
         for (int i = 0; i < n; i++)
         {
             cin >> a[i];
+        }
 
-            if (a[i] > 0)
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] >= 0)
             {
-                count_p++;
                 sum += a[i];
+                continue;
             }
-            else if (a[i] < 0)
+            while (i < n && a[i] <= 0)
             {
                 sum += abs(a[i]);
-                count_n++;
+                i++;
             }
+            count++;
+            i--;
         }
 
-        if (check1(a, n) || check2(a, n))
-        {
-            cout << sum << " " << 1 << endl;
-        }
-        else
-        {
-            cout << sum << " " << min(count_n, count_p + 1) << endl;
-        }
+        cout << sum << " " << count << endl;
     }
 
     return 0;
