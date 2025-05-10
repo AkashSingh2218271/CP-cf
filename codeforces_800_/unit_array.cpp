72807cf6b20a231a4fced49e329d02d94c912f7a
AkashSingh2218271 <akrawat0056@gmail.com>
2025-05-10 21:29:16 +0530

    modified unit_array file

--- codeforces_800_/unit_array.cpp
+++ codeforces_800_/unit_array.cpp
@@ -10,13 +10,13 @@ int main() {
     {
         int n;
         cin >> n;
-        vector<int> a(n);
         int pos_count = 0, neg_count = 0;
 
         for (int i = 0; i < n; i++)
         {
-            cin >> a[i];
-            if (a[i] > 0) {
+            int x;
+            cin >> x;
+            if (x > 0) {
                 pos_count++;
             } else {
                 neg_count++;
@@ -28,7 +28,7 @@ int main() {
         } else {
             int diff = (neg_count - pos_count + 1) / 2;
             neg_count -= diff;
-            if (diff % 2) { diff++; }
+            if (neg_count % 2) { diff++; }
             cout << diff << endl;
         }
     }
